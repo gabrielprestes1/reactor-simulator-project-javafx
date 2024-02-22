ac0b983ac23d2291d744f7740810eae4f4a17196
Gabriel Prestes <darkprestes10@gmail.com>
2024-02-22 09:07:05 -0300

    Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,36 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+
+	private static Scene mainScene;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/views/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
