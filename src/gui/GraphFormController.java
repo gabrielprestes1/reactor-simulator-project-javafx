360959c3ed4051e0ddf32ba2ce96ebf1fc21daad
Gabriel Prestes <darkprestes10@gmail.com>
2024-08-06 18:56:33 -0300

    update simulation with Cantera python

--- src/gui/GraphFormController.java
+++ src/gui/GraphFormController.java
@@ -1,66 +1,74 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import model.entities.Reactor;
 
-import java.text.DecimalFormat;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class GraphFormController {
 
     @FXML
-    private LineChart<?, ?> chartTemp;
+    private LineChart<String, Number> lineChart;
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
+    private String csvFilePath = "C:\\Users\\Gabri\\project-tcc\\reactor-simulator-project-javafx\\src\\model\\simulator\\Dados.csv"; // Substitua pelo caminho real do CSV
 
     @FXML
-    private LineChart<?, ?> chartCon;
+    private void initialize() {
 
-    //= new LineChart<>(new NumberAxis(), new NumberAxis());
-
-
-    public void setResults(List<List<Double>> reults, List<String> equacoes, Integer particoes, Reactor reactor){
-
-        Locale.setDefault(Locale.US);
-
-        DecimalFormat formatter = new DecimalFormat("#0.00000");
+        xAxis.setLabel("Distance");
+        yAxis.setLabel("Value");
 
 
-        XYChart.Series caXZ = new XYChart.Series();
-        XYChart.Series cbXZ = new XYChart.Series();
-        XYChart.Series ccXZ = new XYChart.Series();
-        //XYChart.Series cdXZ = new XYChart.Series();
-        //XYChart.Series TXZ = new XYChart.Series();
-
-
-        for (int i = 0; i < reults.get(reults.size() - 1).size() / (equacoes.size()); i++) {
-
-            Double exp = Double.parseDouble(formatter.format(i*reactor.getL() / Double.valueOf(particoes)));
-            Double Ca = Double.parseDouble(formatter.format(reults.get(reults.size() - 1).get(equacoes.size() * i)));
-            Double Cb = Double.parseDouble(formatter.format(reults.get(reults.size() - 1).get(equacoes.size() * i + 1)));
-            Double Cc = Double.parseDouble(formatter.format(reults.get(reults.size() - 1).get(equacoes.size() * i + 2)));
-            //Double Cd = Double.parseDouble(formatter.format(reults.get(reults.size() - 1).get(equacoes.size() * i + 3)));
-            //Double T = Double.parseDouble(formatter.format(reults.get(reults.size() - 1).get(equacoes.size() * i + 2)));
-
-
-            caXZ.getData().add(new XYChart.Data(exp.toString(), Ca));
-            cbXZ.getData().add(new XYChart.Data(exp.toString(), Cb));
-            ccXZ.getData().add(new XYChart.Data(exp.toString(), Cc));
-            //cdXZ.getData().add(new XYChart.Data(exp.toString(), Cd));
-            //TXZ.getData().add(new XYChart.Data(exp.toString(), T));
-
+        try {
+            loadCSVData();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        caXZ.setName("A Concentration");
-        cbXZ.setName("B Concentration");
-        ccXZ.setName("C Concentration");
-        //cdXZ.setName("D Concentration");
-        //TXZ.setName("Temperatura K");
-
-        chartCon.getData().addAll(caXZ, cbXZ, ccXZ);
-        //chartTemp.getData().add(TXZ);
-
     }
 
+    private void loadCSVData() throws IOException {
+        File file = new File(csvFilePath);
+        if (!file.exists()) {
+            System.out.println("CSV file not found!");
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            boolean firstLine = true;
+            String[] headers = null;
+            XYChart.Series<String, Number> temperatureSeries = new XYChart.Series<>();
+            temperatureSeries.setName("Temperature");
+
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    headers = line.split(",");
+                    firstLine = false;
+                    continue;
+                }
+
+                String[] values = line.split(",");
+                if (values.length > 0) {
+                    String distance = values[0];
+                    double temperature = Double.parseDouble(values[1]); // Supondo que temperatura é a segunda coluna
+
+                    temperatureSeries.getData().add(new XYChart.Data<>(distance, temperature));
+                }
+            }
+
+            lineChart.getData().add(temperatureSeries);
+        }
+    }
 }
+
--- src/gui/PFRFormController.java
+++ src/gui/PFRFormController.java
@@ -1,364 +1,118 @@
 package gui;
 
-import gui.util.Alerts;
-import gui.util.Utils;
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.*;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Properties;
+
+import static java.lang.Double.parseDouble;
 
 
-public class PFRFormController implements Initializable {
-
-    @FXML
-    private Button insertButton, nextButton, exitButton, saveButton;
-
-    @FXML
-    private TextField textReaction, textConcA, textConcC, textConcB, textConcD, textFrequencyFactor, textActivationEnergy;
+public class PFRFormController {
 
     @FXML
-    private TextField width, flow, density, diffusion, initialTemperature, specificHeat, coefficientHeat, deltaH;
+    private ChoiceBox<String> yamlFileChoiceBox;
+    @FXML
+    private Button changeButton;
+    @FXML
+    private TextField compositionTextField;
+    @FXML
+    private TextField inflowVelocityTextField;
+    @FXML
+    private TextField lengthTextField;
+    @FXML
+    private TextField diameterTextField;
+    @FXML
+    private TextField initialPressureTextField;
+    @FXML
+    private TextField initialTemperatureTextField;
+    @FXML
+    private Button saveButton;
+
+    private File currentDirectory;
+
+
 
     @FXML
-    private CheckBox abstractCheckBox, isothermalCheckBox, isobaricCheckBox;
+    private void initialize() {
+        // Inicializar o diretório de trabalho
+        currentDirectory = new File("C:\\Users\\Gabri\\project-tcc\\reactor-simulator-project-javafx\\data");
 
-    @FXML
-    private Label concA, concB, concC, concD, frequencyFactor, activationEnergy, textInitialTemperature, textSpecificHeat;
+        // Atualizar a lista de arquivos no ChoiceBox
+        updateYamlFileList();
 
-    @FXML
-    private Label textCoefficientHeat, textDeltaH;
+        // Configurar ação do botão "Change"
+        changeButton.setOnAction(event -> openDirectory());
+    }
 
-    @FXML
-    private Label un1, un2, un3, un4, un5, un6, un7, un8, un9;
-
-    @FXML
-    private TabPane tabPane;
-
-    @FXML
-    private Tab reaction, getData;
-
-    private String kinetics = "";
-    private Integer numberCompounds = 0;
-    private double CA0;
-    private double CB0;
-    private double CC0;
-    private double CD0;
-    private double K0;
-    private double Ea;
-    private double L;
-    private double Q;
-    private double rho;
-    private double Dab;
-
-
-    Utils util = new Utils();
-
-    @FXML
-    private void onInsertButtonAction() {
-
-        String reaction = textReaction.getText();
-
-        if (reaction.contains("->")) {
-
-            startLabels();
-
-            concA.setVisible(true);
-            concB.setVisible(true);
-            textConcB.setVisible(true);
-            textConcA.setVisible(true);
-            un1.setVisible(true);
-            un2.setVisible(true);
-            frequencyFactor.setVisible(true);
-            textFrequencyFactor.setVisible(true);
-
-            String[] parts = reaction.split("->");
-
-            if (parts.length != 2) {
-                Alerts.showAlert("Error", "Error", "Invalid reaction format", Alert.AlertType.ERROR);
+    private void updateYamlFileList() {
+        if (currentDirectory.isDirectory()) {
+            File[] files = currentDirectory.listFiles((dir, name) -> name.endsWith(".yaml"));
+            if (files != null) {
+                yamlFileChoiceBox.getItems().clear();
+                Arrays.stream(files)
+                        .map(File::getName)
+                        .forEach(yamlFileChoiceBox.getItems()::add);
             }
-
-            String reagents = parts[0].trim();
-            String products = parts[1].trim();
-
-            if (util.validateNumberOfCompounds(reaction)) {
-                Alerts.showAlert("Error", "Error", "Maximum of 4 compounds allowed in the reaction", Alert.AlertType.ERROR);
-                return;
-            }
-
-            if (abstractCheckBox.isSelected()) {
-
-                List<Character> reagentsList = util.extractLetters(reagents);
-                List<Character> productsList = util.extractLetters(products);
-                List<Integer> reagentsNumber = util.extractNumber(reagents);
-
-                numberCompounds = reagentsNumber.size();
-
-                if (reagentsList.size() == 1){
-
-                    concA.setText("Initial Concentration of " + reagentsList.get(0));
-                    concB.setText("Initial Concentration of " + productsList.get(0));
-
-                    kinetics = "[A0](t,z)^" + reagentsNumber.get(0).toString();
-                    if (productsList.size() == 2){
-                        concB.setText("Initial Concentration of " + productsList.get(0));
-                        concC.setText("Initial Concentration of " + productsList.get(1));
-
-                        concC.setVisible(true);
-                        textConcC.setVisible(true);
-                        un3.setVisible(true);
-                        
-                    } else if (productsList.size() == 3) {
-                        concB.setText("Initial Concentration of " + productsList.get(0));
-                        concC.setText("Initial Concentration of " + productsList.get(1));
-                        concD.setText("Initial Concentration of " + productsList.get(2));
-
-                        concD.setVisible(true);
-                        textConcD.setVisible(true);
-                        un4.setVisible(true);
-                    }
-                } else if (reagentsList.size() == 2 ) {
-                    concA.setText("Initial Concentration of " + reagentsList.get(0));
-                    concB.setText("Initial Concentration of " + reagentsList.get(1));
-                    concC.setText("Initial Concentration of " + productsList.get(0));
-
-                    concC.setVisible(true);
-                    textConcC.setVisible(true);
-                    un3.setVisible(true);
-
-                    kinetics = "([A0](t,z)^" + reagentsNumber.get(0).toString() + ")*([B0](t,z)^" + reagentsNumber.get(1).toString() + ")";
-                    if (productsList.size() == 2) {
-                        concC.setText("Initial Concentration of " + productsList.get(0));
-                        concD.setText("Initial Concentration of " + productsList.get(1));
-
-                        concD.setVisible(true);
-                        textConcD.setVisible(true);
-                        un4.setVisible(true);
-                    }
-                } else if (reagentsList.size() == 3) {
-                    concA.setText("Initial Concentration of " + reagentsList.get(0));
-                    concB.setText("Initial Concentration of " + reagentsList.get(1));
-                    concC.setText("Initial Concentration of " + reagentsList.get(2));
-                    concD.setText("Initial Concentration of " + productsList.get(0));
-
-                    concC.setVisible(true);
-                    textConcC.setVisible(true);
-                    un3.setVisible(true);
-                    concD.setVisible(true);
-                    textConcD.setVisible(true);
-                    un4.setVisible(true);
-
-                    kinetics = "([A0](t,z)^" + reagentsNumber.get(0).toString() + ")*([B0](t,z)^" + reagentsNumber.get(1).toString()
-                               + ")*([C0](t,z)^" + reagentsNumber.get(2).toString() +  ")";
-                }
-
-            } else {
-
-                Map<String, Double> reagentsMap = util.parseCompounds(reagents);
-                Map<String, Double> productsMap = util.parseCompounds(products);
-
-                if (util.isBalanced(reagentsMap, productsMap)) {
-                    Alerts.showAlert("Balanced", "Balanced", "The reaction is balanced", Alert.AlertType.INFORMATION);
-                } else {
-                    Alerts.showAlert("Unbalanced", "Unbalanced", "The reaction is unbalanced", Alert.AlertType.WARNING);
-                }
-            }
-        } else if (reaction.contains("<>")) {
-            // Handle reversible reaction similarly
-        } else {
-            Alerts.showAlert("Error", "Error", "Invalid reaction format", Alert.AlertType.ERROR);
         }
+    }
 
-        if (!isothermalCheckBox.isSelected()) {
-            activationEnergy.setVisible(true);
-            textActivationEnergy.setVisible(true);
-            un5.setVisible(true);
+    private void openDirectory() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(currentDirectory);
+        File selectedDirectory = directoryChooser.showDialog(null);
+
+        if (selectedDirectory != null) {
+            currentDirectory = selectedDirectory;
+            updateYamlFileList();
         }
-
     }
 
     @FXML
-    private void onNextButton(){
+    private void onSaveButton() throws IOException {
 
-        tabPane.getSelectionModel().selectNext();
+        SimulationData data = new SimulationData();
 
-        if (reaction.isSelected()){
-            nextButton.setText("Next");
-            tabPane.getSelectionModel().selectNext();
-        }
-
-        if (getData.isSelected()){
-            nextButton.setText("Back");
-            tabPane.getSelectionModel().selectPrevious();
-        }
-
-    }
-
-    @FXML
-    private void onSaveButton(){
-
-        if (textReaction.getText().isEmpty() ||
-                textConcA.getText().isEmpty() ||
-                textConcB.getText().isEmpty() ||
-                textFrequencyFactor.getText().isEmpty() ||
-                (textConcC.isVisible() && textConcC.getText().isEmpty()) ||
-                (textConcD.isVisible() && textConcD.getText().isEmpty()) ||
-                (activationEnergy.isVisible() && activationEnergy.getText().isEmpty())) {
-
-            Alerts.showAlert("Error", "Error", "All fields must be filled", Alert.AlertType.ERROR);
-        }
-
-        CA0 = Double.parseDouble(textConcA.getText());
-        CB0 = Double.parseDouble(textConcB.getText());
-        K0 = Double.parseDouble(textFrequencyFactor.getText());
-
-        if (textConcC.isVisible()){
-            CC0 = Double.parseDouble(textConcB.getText());
-        } else if (textConcD.isVisible()){
-            CC0 = Double.parseDouble(textConcB.getText());
-            CD0 = Double.parseDouble(textConcD.getText());
-        }
-
-        if (textActivationEnergy.isVisible()){
-            Ea = Double.parseDouble(textActivationEnergy.getText());
-        }
-        /*
-            if (width.getText().isEmpty() ||
-                    flow.getText().isEmpty() ||
-                    density.getText().isEmpty() ||
-                    diffusion.getText().isEmpty() ||
-                    (initialTemperature.isVisible() && initialTemperature.getText().isEmpty()) ||
-                    (specificHeat.isVisible() && specificHeat.getText().isEmpty()) ||
-                    (coefficientHeat.isVisible() && coefficientHeat.getText().isEmpty()) ||
-                    (deltaH.isVisible() && deltaH.getText().isEmpty())) {
-
-                Alerts.showAlert("Error", "Error", "All fields must be filled", Alert.AlertType.ERROR);
-            }
-
-             */
-
-        L = Double.parseDouble(width.getText());
-        Q = Double.parseDouble(flow.getText());
-        rho = Double.parseDouble(density.getText());
-        Dab = Double.parseDouble(diffusion.getText());
+        String inputFilePath = "C:\\Users\\Gabri\\project-tcc\\reactor-simulator-project-javafx\\src\\model\\simulator\\input.json";
+        writeDataToFile(data, inputFilePath);
 
         Stage stage = (Stage) saveButton.getScene().getWindow();
         stage.close();
-
     }
 
-
-    @FXML
-    private void onExitButton() {
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    private void onIsobaricPress(){
-        if (isobaricCheckBox.isSelected()) {
-            startLabels();
-        } else {
-            textInitialTemperature.setVisible(false);
-            textSpecificHeat.setVisible(false);
-            textCoefficientHeat.setVisible(false);
-            textDeltaH.setVisible(false);
-            initialTemperature.setVisible(false);
-            specificHeat.setVisible(false);
-            coefficientHeat.setVisible(false);
-            deltaH.setVisible(false);
-            un6.setVisible(false);
-            un7.setVisible(false);
-            un8.setVisible(false);
-            un9.setVisible(false);
+    private Double parseDouble(String text) {
+        try {
+            return text != null && !text.trim().isEmpty() ? Double.parseDouble(text.trim()) : null;
+        } catch (NumberFormatException e) {
+            return null;
         }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        startLabels();
+    private void writeDataToFile(SimulationData data, String filePath) throws IOException {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(data, writer);
+        }
     }
 
-    private void startLabels(){
-        concA.setVisible(false);
-        concB.setVisible(false);
-        concC.setVisible(false);
-        concD.setVisible(false);
-        activationEnergy.setVisible(false);
-        frequencyFactor.setVisible(false);
-        textConcA.setVisible(false);
-        textConcB.setVisible(false);
-        textConcC.setVisible(false);
-        textConcD.setVisible(false);
-        textActivationEnergy.setVisible(false);
-        textFrequencyFactor.setVisible(false);
-        un1.setVisible(false);
-        un2.setVisible(false);
-        un3.setVisible(false);
-        un4.setVisible(false);
-        un5.setVisible(false);
+    private class SimulationData {
+        String yamlFile = yamlFileChoiceBox.getValue();
+        String composition = compositionTextField.getText();
+        String yamlFilePath = new File("C:\\Users\\Gabri\\project-tcc\\reactor-simulator-project-javafx\\data", yamlFile).getAbsolutePath();;
 
-        textInitialTemperature.setVisible(false);
-        textSpecificHeat.setVisible(false);
-        textCoefficientHeat.setVisible(false);
-        textDeltaH.setVisible(false);
-        initialTemperature.setVisible(false);
-        specificHeat.setVisible(false);
-        coefficientHeat.setVisible(false);
-        deltaH.setVisible(false);
-        un6.setVisible(false);
-        un7.setVisible(false);
-        un8.setVisible(false);
-        un9.setVisible(false);
+        Double inflowVelocity = parseDouble(inflowVelocityTextField.getText());
+        Double length = parseDouble(lengthTextField.getText());
+        Double diameter = parseDouble(diameterTextField.getText());
+        Double initialPressure = parseDouble(initialPressureTextField.getText());
+        Double initialTemperature = parseDouble(initialTemperatureTextField.getText());
     }
 
-    public String getKinetics() {
-        return kinetics;
-    }
-
-    public Integer getNumberCompounds() {
-        return numberCompounds;
-    }
-
-    public Double getCA0() {
-        return CA0;
-    }
-
-    public Double getCB0() {
-        return CB0;
-    }
-
-    public Double getCC0() {
-        return CC0;
-    }
-
-    public Double getCD0() {
-        return CD0;
-    }
-
-    public Double getK0() {
-        return K0;
-    }
-
-    public Double getEa() {
-        return Ea;
-    }
-
-    public Double getDab() {
-        return Dab;
-    }
-
-    public Double getRho() {
-        return rho;
-    }
-
-    public Double getQ() {
-        return Q;
-    }
-
-    public Double getL() {
-        return L;
-    }
 }
